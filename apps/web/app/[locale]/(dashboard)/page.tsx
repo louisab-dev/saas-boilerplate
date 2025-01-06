ce4d26737b29b9e9b6f040adf2a377ba5a23c48c
Louis Amoros <lamoros@bmat.com>
2025-01-06 14:06:34 +0100

    chore: better layout for example components

--- apps/web/app/[locale]/(dashboard)/page.tsx
+++ apps/web/app/[locale]/(dashboard)/page.tsx
@@ -2,7 +2,7 @@ import { ExampleComponent } from "@/components/example-component";
 import { ServerExampleComponent } from "@/components/server-example-component";
 export default function Home() {
   return (
-    <div>
+    <div className="flex flex-col justify-center gap-4 p-4">
       <ExampleComponent />
       <ServerExampleComponent />
     </div>
