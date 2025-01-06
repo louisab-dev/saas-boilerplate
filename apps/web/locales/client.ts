f5c92dec504f89ce0c24e11d23037f61300a8d7a
Louis Amoros <lamoros@bmat.com>
2025-01-06 15:52:06 +0100

    chore: fix next-international export issue

--- apps/web/locales/client.ts
+++ apps/web/locales/client.ts
@@ -1,4 +1,7 @@
 "use client";
+
+// This is needed as a workaround: https://github.com/microsoft/TypeScript/issues/47663#issuecomment-1519138189
+import * as core from "next-international";
 import { createI18nClient } from "next-international/client";
 
 export const { useI18n, useScopedI18n, I18nProviderClient } = createI18nClient({
