066c751f72eed3f8db6362b63495b66d73f53dca
Louis Amoros <lamoros@bmat.com>
2025-01-06 04:00:35 +0100

    chore: simpler layout for root layout

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -4,8 +4,6 @@ import "./globals.css";
 import { TRPCProvider } from "@/lib/trpc/Context";
 import { ThemeProvider } from "@/components/theme/theme-provider";
 import { PublicEnvScript } from "next-runtime-env";
-import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
-import { AppSidebar } from "@/components/sidebar/app-sidebar";
 
 export const metadata: Metadata = {
   title: "CHANGEME",
@@ -24,21 +22,14 @@ export default function RootLayout({
       </head>
       <body>
         <TRPCProvider>
-          <SidebarProvider>
-            <ThemeProvider
-              attribute="class"
-              defaultTheme="system"
-              enableSystem
-              disableTransitionOnChange
-            >
-              <AppSidebar />
-              <SidebarInset>
-                <div className="flex flex-1 flex-col gap-4 p-4">
-                  {children}
-                </div>
-              </SidebarInset>
-            </ThemeProvider>
-          </SidebarProvider>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
         </TRPCProvider>
       </body>
     </html>
