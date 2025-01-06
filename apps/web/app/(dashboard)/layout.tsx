f1111d79748f40cd72ad83a811bed8b48c83c820
Louis Amoros <lamoros@bmat.com>
2025-01-06 04:00:11 +0100

    chore: add dashboard layout

---
+++ apps/web/app/(dashboard)/layout.tsx
@@ -0,0 +1,25 @@
+import { Metadata } from "next";
+import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
+import { AppSidebar } from "@/components/sidebar/app-sidebar";
+
+export const metadata: Metadata = {
+  title: "CHANGEME",
+  description: "CHANGEME",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <SidebarProvider>
+      <AppSidebar />
+      <SidebarInset>
+        <div className="flex flex-1 flex-col gap-4 p-4">
+          {children}
+        </div>
+      </SidebarInset>
+    </SidebarProvider>
+  );
+}
---
+++ apps/web/app/(dashboard)/page.tsx
@@ -0,0 +1,10 @@
+import { ExampleComponent } from "@/components/example-component";
+import { ServerExampleComponent } from "@/components/server-example-component";
+export default function Home() {
+  return (
+    <div>
+      <ExampleComponent />
+      <ServerExampleComponent />
+    </div>
+  );
+}
