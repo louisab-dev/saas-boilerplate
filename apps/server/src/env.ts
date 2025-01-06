5a590eeb134067dce849814ad7554e652ce0051d
Louis Amoros <lamoros@bmat.com>
2025-01-06 01:24:40 +0100

    chore: simpler server env management

--- apps/server/src/env.ts
+++ apps/server/src/env.ts
@@ -1,10 +1,4 @@
 import { z } from "zod";
-import dotenv from "dotenv";
-import path from "path";
-
-dotenv.config({
-  path: path.resolve(process.cwd(), ".env"),
-});
 
 const envSchema = z.object({
   NODE_ENV: z.enum(["development", "production", "test"]),
