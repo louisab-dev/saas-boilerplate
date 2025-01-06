1405c8da360bf4cfc9d1b0f7b4bdeeb4cc93e3da
Louis Amoros <lamoros@bmat.com>
2025-01-06 03:15:29 +0100

    chore: better auth handling with hooks

--- apps/web/hooks/use-auth.ts
+++ apps/web/hooks/use-auth.ts
@@ -1,33 +1,60 @@
 import { useState } from "react";
 import { trpc } from "@/lib/trpc/client";
 import { useToast } from "./use-toast";
+import { supabase } from "@/lib/auth/client";
+import { useRouter } from "next/navigation";
 
 export const useAuth = () => {
   const { toast } = useToast();
   const [loading, setLoading] = useState(false);
-  const signup = trpc.auth.signup.useMutation({
-    onSuccess: () =>
-      toast({
-        title: "Success",
-        description: "You have successfully signed up",
-        variant: "success",
-      }),
-    onError: (error: any) =>
-      toast({
-        title: "Error",
-        description: error.message,
-        variant: "destructive",
-      }),
-  });
+  const router = useRouter();
 
   const signUpWithEmail = async (email: string, password: string) => {
+    const signup = trpc.auth.signup.useMutation({
+      onSuccess: () =>
+        toast({
+          title: "Success",
+          description: "You have successfully signed up",
+          variant: "success",
+        }),
+      onError: (error: any) =>
+        toast({
+          title: "Error",
+          description: error.message,
+          variant: "destructive",
+        }),
+    });
     setLoading(true);
     await signup.mutateAsync({ email, password });
     setLoading(false);
   };
 
+  const signOut = async () => {
+    await supabase.auth.signOut();
+    router.push("/");
+  };
+
+  const signInWithEmail = async (email: string, password: string) => {
+    const { error } = await supabase.auth.signInWithPassword({
+      email: email,
+      password: password,
+    });
+
+    if (error) {
+      toast({
+        title: "Error",
+        description: error.message,
+        variant: "destructive",
+      });
+    } else {
+      router.push("/");
+    }
+  };
+
   return {
     loading,
     signUpWithEmail,
+    signOut,
+    signInWithEmail,
   };
 };
