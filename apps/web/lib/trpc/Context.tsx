1ba0200dc157219d8b0c00e440f3ed64f106f6d1
Louis Amoros <lamoros@bmat.com>
2025-01-07 11:07:13 +0100

    feat: add supabase headers

--- apps/web/lib/trpc/Context.tsx
+++ apps/web/lib/trpc/Context.tsx
@@ -5,6 +5,7 @@ import React, { useState } from "react";
 
 import superJson from "superjson";
 import { trpc } from "./client";
+import { supabase } from "@/lib/auth/client";
 
 import { env } from "next-runtime-env"; // This is a workaround that makes all the env variables prefixed with NEXT_PUBLIC to be accessible from the browser. I'd like to get rid of this dependency by following the correct way to load a public env var like the API_URL but I could not find it in the docs. This is worth looking into: https://github.com/vercel/next.js/discussions/17641#discussioncomment-11461646
 
@@ -16,10 +17,16 @@ export function TRPCProvider({ children }: { children: React.ReactNode }) {
       links: [
         httpBatchLink({
           url: `${API_URL}/api/trpc`,
+          headers: async () => {
+            const { data: { session } } = await supabase.auth.getSession();
+            return {
+              Authorization: `Bearer ${session?.access_token}`,
+            };
+          },
         }),
       ],
       transformer: superJson,
-    }),
+    })
   );
   return (
     <trpc.Provider client={trpcClient} queryClient={queryClient}>
